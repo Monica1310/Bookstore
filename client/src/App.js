a247d9203bed20532fa7bb90bfab97da478bfe20
Monica1310 <168436554+Monica1310@users.noreply.github.com>
2024-07-16 14:09:00 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Books from './Books';
-import AddBook from './AddBook'; // Import AddBook component
+// import AddBook from './AddBook'; // Import AddBook component
+import Authors from './Authors'; // Import the Authors component
+import Genres from './Genres'; 
 import { Link } from 'react-router-dom';
+import AddBook from './AddBook';
+
 
 function App() {
   return (
     <Router>
-      <div>
+      
         <Navbar />
+        <div className="app-content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books />} />
-          <Route path="/addbook" element={<AddBook />} /> {/* Add route for AddBook */}
+          <Route path="/addbook" element={<AddBook/>} /> {/* Add route for AddBook */}
+          <Route path="/authors" element={<Authors />} /> {/* Add Authors route */}
+          <Route path="/genres" element={<Genres />} /> {/* Add Authors route */}
         </Routes>
       </div>
     </Router>
